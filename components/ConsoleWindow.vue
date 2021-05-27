71c7ffcc78eb66278802287743af6d76c0525616
Marius Andresen <marius.e.g.a@gmail.com>
2021-05-27 15:19:33 +0200

    Moved the DateTime, Dock and Condole into their own components

---
+++ components/ConsoleWindow.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="
+      z-10
+      h-5/6
+      w-1/2
+      absolute
+      flex flex-col
+      bg-gray-900 bg-opacity-90
+      rounded-lg
+      shadow-lg
+    "
+  >
+    <div
+      class="
+        z-20
+        flex
+        justify-between
+        bg-gray-900
+        rounded-t-lg
+        px-2
+        py-1
+        text-gray-400 text-sm
+      "
+    >
+      <div class="flex items-center space-x-1.5 text-gray-700 fill-current">
+        <icon-circle class="h-3 transition hover:text-red-500" />
+        <icon-circle class="h-3 transition hover:text-yellow-500" />
+        <icon-circle class="h-3 transition hover:text-green-500" />
+      </div>
+      <div class="select-none">Portfolio</div>
+      <div class="select-none"></div>
+    </div>
+    <div
+      class="flex flex-col flex-grow overflow-auto p-2 text-gray-400 font-mono"
+    >
+      <div>
+        <ul>
+          <li v-html="commands.help" />
+        </ul>
+      </div>
+      <div>
+        <span>kalosar@macbook></span>
+        <span>{{ userInput }}</span>
+        <span class="animate-ping font-bold">_</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConsoleWindow',
+  data() {
+    return {
+      userInput: '',
+      commands: {
+        help:
+          '<p>Commands</p>\n' +
+          '<p>--------</p>\n' +
+          '<p>[h]|[help] ------- Print list of commands</p>\n' +
+          '<p>[i]|[info] ------- Print info about me</p>\n' +
+          '<p>[s]|[skills] ----- List skills</p>\n' +
+          '<p>[e]|[experience] - List experience</p>\n' +
+          '<br />\n' +
+          '</div>',
+      },
+    }
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ components/Dock.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="
+      flex
+      items-center
+      py-2
+      px-6
+      mb-2
+      bg-gray-900 bg-opacity-90
+      rounded-xl
+      space-x-4
+    "
+  >
+    <a href="#" class="dock-button bg-black">
+      <icon-github class="text-white fill-current" />
+    </a>
+    <a href="#" class="dock-button bg-blue-500">
+      <icon-linkedin class="text-white fill-current" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dock',
+}
+</script>
+
+<style scoped>
+.dock-button {
+  @apply h-14 w-14 p-2 rounded-lg transform transition duration-500 hover:scale-110;
+}
+</style>
