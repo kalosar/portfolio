7e97af77d76a32f01c8550e7d5f7e581b3188fcf
Marius Andresen <marius.e.g.a@gmail.com>
2021-05-27 15:17:54 +0200

    Moved SVG icons into Vue components

---
+++ components/IconCircle.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+    <circle cx="32" cy="32" r="32" stroke-width="0" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconCircle',
+}
+</script>
+
+<style scoped></style>
---
+++ components/IconClock.vue
@@ -0,0 +1,46 @@
+<template>
+  <svg
+    v-if="type === 'outline'"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+  <svg
+    v-else-if="type === 'solid'"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconClock',
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'outline',
+      validator(value) {
+        return value === 'outline' || value === 'solid'
+      },
+    },
+  },
+}
+</script>
+
+<style scoped></style>
